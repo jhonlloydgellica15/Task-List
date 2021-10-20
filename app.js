e43001e1f907e6e888df40920726ccde1d4c87f8
JhonLloyd <jhonlloydgellica15@gmail.com>
2021-10-20 14:53:46 +0800

    store to local storage

--- app.js
+++ app.js
@@ -11,11 +11,41 @@ loadEventListeners();
 
 //Load all event listeners
 function loadEventListeners() {
+  document.addEventListener("DOMContentLoaded", getTasks);
+
   form.addEventListener("submit", addTask);
 
   taskList.addEventListener("click", removeTask);
 
   clearBtn.addEventListener("click", clearTasks);
+
+  filter.addEventListener("keyup", filterTask);
+}
+//Get Tasks from LS
+function getTasks() {
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task) {
+    const li = document.createElement("li");
+    //add class
+    li.className = "collection-item";
+    //Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+
+    //Create new link element
+    const link = document.createElement("a");
+    //Add class
+    link.className = "delete-item secondary-content";
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append link to li
+    li.appendChild(link);
+    taskList.appendChild(li);
+  });
 }
 
 //add task
@@ -23,25 +53,43 @@ function addTask(e) {
   e.preventDefault();
 
   if (taskInput.value === "") alert("Add a task");
+  else {
+    //Create li
+    const li = document.createElement("li");
+    //add class
+    li.className = "collection-item";
+    //Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
 
-  //Create li
-  const li = document.createElement("li");
-  //add class
-  li.className = "collection-item";
-  //Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
+    //Create new link element
+    const link = document.createElement("a");
+    //Add class
+    link.className = "delete-item secondary-content";
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append link to li
+    li.appendChild(link);
 
-  //Create new link element
-  const link = document.createElement("a");
-  //Add class
-  link.className = "delete-item secondary-content";
-  // Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //append link to li
-  li.appendChild(link);
+    storeTaskInLocalStorage(taskInput.value);
 
-  taskInput.value = "";
-  taskList.appendChild(li);
+    taskInput.value = "";
+    taskList.appendChild(li);
+  }
+
+  //Store task
+  function storeTaskInLocalStorage(task) {
+    let tasks;
+
+    if (localStorage.getItem("tasks") === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 }
 
 //Remove Task
@@ -49,12 +97,56 @@ function removeTask(e) {
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
+
+      //Remove from LS
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
 }
 
+//Remove from storage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+//Clear
 function clearTasks() {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+
+  //Clear from LS
+
+  clearTasksFromLocalStorage();
+}
+
+//Clear
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+//Filter  tasks
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
